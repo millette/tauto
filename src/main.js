d17794de99bbc953e79dbda87be1ac166ccca7dc
Robin Millette <robin@millette.info>
2019-08-20 22:19:49 -0400

    Prettier.

--- src/main.js
+++ src/main.js
@@ -1,62 +1,70 @@
 // npm
-import React, { Component } from 'react';
-import ReactDOM, { findDOMNode } from 'react-dom';
-import TextField from 'react-autocomplete-input';
-import MiniSearch from 'minisearch';
+import React, { Component } from "react"
+import ReactDOM, { findDOMNode } from "react-dom"
+import TextField from "react-autocomplete-input"
+import MiniSearch from "minisearch"
 
 // self
 import idx from "./inventory-idx.json"
 
-const searchEngine = MiniSearch.loadJSON(JSON.stringify(idx), { fields: ["h2", "summary"] })
+const searchEngine = MiniSearch.loadJSON(JSON.stringify(idx), {
+  fields: ["h2", "summary"],
+})
 
 class App extends Component {
   constructor() {
-    super();
+    super()
 
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDisable = this.handleDisable.bind(this);
-    this.handleMaxOptionChange = this.handleMaxOptionChange.bind(this);
-    this.handleRegexChange = this.handleRegexChange.bind(this);
-    this.handleRequestOnlyIfNoOptions = this.handleRequestOnlyIfNoOptions.bind(this);
-    this.handleRequestOptions = this.handleRequestOptions.bind(this);
-    this.handleSpaceRemoversChange = this.handleSpaceRemoversChange.bind(this);
-    this.handleSpacerChange = this.handleSpacerChange.bind(this);
-    this.handleTriggerChange = this.handleTriggerChange.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this)
+    this.handleDisable = this.handleDisable.bind(this)
+    this.handleMaxOptionChange = this.handleMaxOptionChange.bind(this)
+    this.handleRegexChange = this.handleRegexChange.bind(this)
+    this.handleRequestOnlyIfNoOptions = this.handleRequestOnlyIfNoOptions.bind(
+      this
+    )
+    this.handleRequestOptions = this.handleRequestOptions.bind(this)
+    this.handleSpaceRemoversChange = this.handleSpaceRemoversChange.bind(this)
+    this.handleSpacerChange = this.handleSpacerChange.bind(this)
+    this.handleTriggerChange = this.handleTriggerChange.bind(this)
 
     this.state = {
       disabled: false,
       maxOptions: 10,
       options: [],
-      regex: '^[a-zA-Z0-9_\\-]+$',
+      regex: "^[a-zA-Z0-9_\\-]+$",
       requestOnlyIfNoOptions: false,
       spaceRemovers: "[',', '.', '?', '!']",
       spacer: " ",
-      trigger: '' // '@'
-    };
+      trigger: "", // '@'
+    }
   }
 
   handleAddOption() {
-    const nextOptions = this.state.options;
+    const nextOptions = this.state.options
 
-    nextOptions.push(findDOMNode(this.refOptionField).value.trim());
-    this.setState({ options: nextOptions });
+    nextOptions.push(findDOMNode(this.refOptionField).value.trim())
+    this.setState({ options: nextOptions })
   }
 
   handleDisable() {
-    this.setState({ disabled: !this.state.disabled });
+    this.setState({ disabled: !this.state.disabled })
   }
 
   handleMaxOptionChange(e) {
-    this.setState({ maxOptions: e.target.value });
+    this.setState({ maxOptions: e.target.value })
   }
 
   handleRegexChange(e) {
-    this.setState({ regex: e.target.value });
+    this.setState({ regex: e.target.value })
   }
 
   handleRequestOptions(str) {
-    console.log(`Requesting options for string: ${str.length} ${str}`);
-    this.setState({ options: searchEngine.autoSuggest(str).map(({ suggestion }) => suggestion) })
+    console.log(`Requesting options for string: ${str.length} ${str}`)
+    this.setState({
+      options: searchEngine
+        .autoSuggest(str)
+        .map(({ suggestion }) => suggestion),
+    })
     // this.setState({options: (str.length) ? searchEngine.autoSuggest(str).map(({ suggestion }) => suggestion) : []})
     /*
     if (str.length > 1)
@@ -66,7 +74,9 @@ class App extends Component {
   }
 
   handleRequestOnlyIfNoOptions(e) {
-    this.setState({ requestOnlyIfNoOptions: !this.state.requestOnlyIfNoOptions });
+    this.setState({
+      requestOnlyIfNoOptions: !this.state.requestOnlyIfNoOptions,
+    })
   }
 
   handleSpaceRemoversChange(e) {
@@ -82,7 +92,9 @@ class App extends Component {
   }
 
   render() {
-    const options = this.state.options.sort((a, b) => a.localeCompare(b)).map(option => <li key={option}>{option}</li>);
+    const options = this.state.options
+      .sort((a, b) => a.localeCompare(b))
+      .map((option) => <li key={option}>{option}</li>)
 
     return (
       <div>
@@ -90,7 +102,7 @@ class App extends Component {
           <h2>AutoCompletion demo</h2>
           <TextField
             disabled={this.state.disabled}
-            style={{ width: '300px', height: '100px', display: 'block' }}
+            style={{ width: "300px", height: "100px", display: "block" }}
             maxOptions={parseInt(this.state.maxOptions, 10)}
             onRequestOptions={this.handleRequestOptions}
             options={this.state.options}
@@ -101,18 +113,20 @@ class App extends Component {
             trigger={this.state.trigger}
           />
         </div>
-        <hr style={{ margin: '20px 0' }} />
+        <hr style={{ margin: "20px 0" }} />
         <h2>Options</h2>
         <div className="option-block">
           <h3>options : array</h3>
           <p>List of autocomplete options</p>
           <p>Default value: []</p>
           <p>Demo options</p>
-          <ul className='options'>
-            {options}
-          </ul>
+          <ul className="options">{options}</ul>
           <div className="field">
-            <input ref={c => { this.refOptionField = c; }} />
+            <input
+              ref={(c) => {
+                this.refOptionField = c
+              }}
+            />
           </div>
           <button onClick={this.handleAddOption}>Add</button>
         </div>
@@ -121,15 +135,24 @@ class App extends Component {
           <p>Show autocomplete option list if trigger string is met.</p>
           <p>Default value: '@'. </p>
           <div className="field">
-            <input onChange={this.handleTriggerChange} value={this.state.trigger} />
+            <input
+              onChange={this.handleTriggerChange}
+              value={this.state.trigger}
+            />
           </div>
         </div>
         <div className="option-block">
           <h3>spaceRemovers : array</h3>
-          <p>If one of this characters is putted after selected option, space is removed. I.e. "@option ,|" -> "@option, |".</p>
+          <p>
+            If one of this characters is putted after selected option, space is
+            removed. I.e. "@option ,|" -> "@option, |".
+          </p>
           <p>Default value: [',', '.', '!', '?'].</p>
           <div className="field">
-            <input onBlur={this.handleSpaceRemoversChange} defaultValue={this.state.spaceRemovers} />
+            <input
+              onBlur={this.handleSpaceRemoversChange}
+              defaultValue={this.state.spaceRemovers}
+            />
           </div>
         </div>
         <div className="option-block">
@@ -137,23 +160,40 @@ class App extends Component {
           <p>Character to add after selected option.</p>
           <p>Default value: ' '.</p>
           <div className="field">
-            <input onBlur={this.handleSpacerChange} defaultValue={this.state.spacer} />
+            <input
+              onBlur={this.handleSpacerChange}
+              defaultValue={this.state.spacer}
+            />
           </div>
         </div>
         <div className="option-block">
           <h3>maxOptions : integer</h3>
-          <p>Maximum number of options provided in the option list. Rest of option list is truncated.</p>
+          <p>
+            Maximum number of options provided in the option list. Rest of
+            option list is truncated.
+          </p>
           <p>Default value: 6</p>
           <div className="field">
-            <input onChange={this.handleMaxOptionChange} value={this.state.maxOptions} />
+            <input
+              onChange={this.handleMaxOptionChange}
+              value={this.state.maxOptions}
+            />
           </div>
         </div>
         <div className="option-block">
           <h3>requestOnlyIfNoOptions : boolean</h3>
-          <p>Control parent calls to get new options. If true calls are executed only if all provided options don't match the pattern. Otherwise, call is performed on every field change after trigger is found.</p>
+          <p>
+            Control parent calls to get new options. If true calls are executed
+            only if all provided options don't match the pattern. Otherwise,
+            call is performed on every field change after trigger is found.
+          </p>
           <p>Default value: true</p>
           <div className="field">
-            <input type="checkbox" onChange={this.handleRequestOnlyIfNoOptions} checked={this.state.requestOnlyIfNoOptions} />
+            <input
+              type="checkbox"
+              onChange={this.handleRequestOnlyIfNoOptions}
+              checked={this.state.requestOnlyIfNoOptions}
+            />
           </div>
         </div>
         <div className="option-block">
@@ -161,7 +201,10 @@ class App extends Component {
           <p>Autocomplete options must match provided regex</p>
           <p>Default value: '^[a-zA-Z0-9\-_]+$'</p>
           <div className="field">
-            <input onBlur={this.handleRegexChange} defaultValue={this.state.regex} />
+            <input
+              onBlur={this.handleRegexChange}
+              defaultValue={this.state.regex}
+            />
           </div>
         </div>
         <div className="option-block">
@@ -169,12 +212,16 @@ class App extends Component {
           <p>Disable field, i.e. during submission</p>
           <p>Default value: false</p>
           <div className="field">
-            <input type="checkbox" onChange={this.handleDisable} checked={this.state.disabled} />
+            <input
+              type="checkbox"
+              onChange={this.handleDisable}
+              checked={this.state.disabled}
+            />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"))
